2737937a45fd18b880053464779c2d72d249cf7a
78552423@qq.com <78552423@qq.com>
2020-09-28 13:45:18 +0800

    getLogger

--- src/adapter/base.js
+++ src/adapter/base.js
@@ -29,8 +29,7 @@ module.exports = class {
   }
 
   getLogger(category) {
-    this[_logger] = log4js.getLogger(category);
-    return this[_logger];
+    return log4js.getLogger(category);
   }
 
   /**
