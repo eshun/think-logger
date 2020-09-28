68a8ca0550f30590a7d7a4d125eecfa0b97e22f9
78552423@qq.com <78552423@qq.com>
2020-09-28 12:58:20 +0800

    show category

--- src/adapter/console.js
+++ src/adapter/console.js
@@ -2,16 +2,21 @@ const Base = require('./base');
 
 module.exports = class ConsoleLogger extends Base {
   formatConfig(config) {
-    let {level, layout} = config;
+    let { level, layout } = config;
     level = level ? level.toUpperCase() : 'ALL';
-    layout = layout || {type: 'pattern', pattern: '%[[%d] [%z] [%p]%] - %m'};
+    //layout = layout || {type: 'pattern', pattern: '%[[%d] [%z] [%p]%] - %m'};
+
+    const props = {};
+    if (!!layout) {
+      props.layout = layout;
+    }
 
     return Object.assign({
       appenders: {
-        console: {type: 'console', layout}
+        console: { type: 'console', ...props }
       },
       categories: {
-        default: {appenders: ['console'], level}
+        default: { appenders: ['console'], level }
       }
     }, config);
   }
